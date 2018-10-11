c6adc834f11b79f9baf2961bcc4de5680a9b3cde
pnganga <pnganga05@gmail.com>
2018-10-11 03:27:46 +0300

    ADD ANKOLE LOGO

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -25,6 +25,14 @@ exports.getOne = function(code, done) {
     });
 }
 
+exports.getOneByMac = function(mac, done) {
+	var value = [mac]
+    db.get().query('SELECT * FROM radcheck WHERE macAddress = ?', value, function(err, row) {
+        if (err) console.log(err);
+        done(null, row)
+    });
+}
+
 exports.getAllByUser = function(userId, done) {
     db.get().query('SELECT * FROM comments WHERE user_id = ?', userId, function(err, rows) {
         if (err) return done(err)
